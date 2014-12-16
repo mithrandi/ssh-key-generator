6a09916fc12595c29e289dd55348ad74782deb13
Tristan Seligmann <mithrandi@mithrandi.net>
2014-12-16 05:19:43 +0200

    Start implementing generation algorithm.

--- Main.hs
+++ Main.hs
@@ -2,16 +2,20 @@
 
 module Main (main) where
 
-import Control.Applicative (pure)
+import qualified Crypto.Hash.SHA256 as SHA256
 import qualified Data.ByteString as B
-import SSH.Key (parseKey, publicKeys, privateKeys, serialiseKey)
+import qualified Data.ByteString.Char8 as BC
+import           Data.Monoid ((<>))
+import           SSH.Key (PrivateKey(Ed25519PrivateKey), PublicKey(Ed25519PublicKey), serialiseKey)
+import           System.Environment (getArgs)
 
 main :: IO ()
 main = do
-  contents <- B.getContents
-  box <- either error pure (parseKey contents)
-  --print box
-  --print $ publicKeys box
-  --print $ privateKeys box
-  let [key] = privateKeys box
-  B.putStr $ serialiseKey key
+  [secretFile, handleS] <- getArgs
+  let handle = BC.pack handleS
+  secret <- B.readFile secretFile
+  let seed = SHA256.hash (secret <> handle)
+  let privateKeyData = undefined
+      publicKeyData = undefined
+  B.putStr . serialiseKey
+    $ Ed25519PrivateKey (Ed25519PublicKey publicKeyData) privateKeyData handle
