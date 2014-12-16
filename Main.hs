da16a136606ae5240b5124991b7456ce7a967324
Tristan Seligmann <mithrandi@mithrandi.net>
2014-12-16 03:09:31 +0200

    Refactor data types and interface.

--- Main.hs
+++ Main.hs
@@ -4,10 +4,12 @@ module Main (main) where
 
 import Control.Applicative (pure)
 import qualified Data.ByteString as B
-import SSH.Key (parseKey)
+import SSH.Key (parseKey, publicKeys, privateKeys)
 
 main :: IO ()
 main = do
   contents <- B.getContents
   box <- either error pure (parseKey contents)
   print box
+  print $ publicKeys box
+  print $ privateKeys box
--- SSH/Key.hs
+++ SSH/Key.hs
@@ -1,23 +1,27 @@
 module SSH.Key
-       ( KeyBox(..)
-       , Key(..)
+       ( KeyBox
+       , PublicKey(..)
+       , PrivateKey(..)
        , parseKey
+       , publicKeys
+       , privateKeys
        ) where
 
-import Control.Applicative ((<$>), (<*>))
-import Control.Monad (unless, replicateM)
+import           Control.Applicative ((<$>), (<*>))
+import           Control.Monad (unless, replicateM)
+import           Data.Binary.Get (Get, runGet, getWord32be, getByteString, getRemainingLazyByteString)
 import qualified Data.ByteString as B
-import qualified Data.ByteString.Char8 as BC
 import qualified Data.ByteString.Base64 as B64
-import Data.ByteString.Lazy (fromStrict, toStrict)
-import Data.Binary.Get (Get, runGet, getWord32be, getByteString, getRemainingLazyByteString)
+import qualified Data.ByteString.Char8 as BC
+import           Data.ByteString.Lazy (fromStrict, toStrict)
 
 data KeyBox = KeyBox
               { ciphername :: B.ByteString
-              , kdfname :: B.ByteString
-              , kdfoptions :: B.ByteString
-              , boxPublicKeys  :: [Key]
-              , boxPrivateKeys :: Maybe [Key]
+              , _kdfname :: B.ByteString
+              , _kdfoptions :: B.ByteString
+              , keycount :: Int
+              , boxPublicKeys :: B.ByteString
+              , boxPrivateKeys :: B.ByteString
               } deriving (Show)
 
 auth_magic :: B.ByteString
@@ -26,18 +30,16 @@ auth_magic = "openssh-key-v1\000"
 expected_padding :: B.ByteString
 expected_padding = BC.pack ['\001'..'\377']
 
-data Key = PublicKey
-           { keyAlg :: B.ByteString
-           , publicKeyData :: B.ByteString
-           }
-         | PrivateKey
-           { privateKeyAlg :: B.ByteString
-           , publicKeyData :: B.ByteString
-           , privateKeyData :: B.ByteString
-           , comment :: B.ByteString
-           }
-         deriving (Show)
+data PublicKey = Ed25519PublicKey
+                 { publicKeyData :: B.ByteString }
+               deriving (Show)
 
+data PrivateKey = Ed25519PrivateKey
+                  { publicKey :: PublicKey
+                  , privateKeyData :: B.ByteString
+                  , privateKeyComment :: B.ByteString
+                  }
+                deriving (Show)
 
 runStrictGet :: Get c -> B.ByteString -> c
 runStrictGet = (. fromStrict) . runGet
@@ -51,10 +53,11 @@ dearmorPrivateKey =
     . dropWhile (/= "-----BEGIN OPENSSH PRIVATE KEY-----")
     . BC.lines
 
+getWord32be' :: (Integral a) => Get a
+getWord32be' = fromIntegral <$> getWord32be
+
 getPascalString :: Get B.ByteString
-getPascalString = do
-  len <- getWord32be
-  getByteString (fromIntegral len)
+getPascalString = getWord32be' >>= getByteString
 
 getKeyBox :: Get KeyBox
 getKeyBox = do
@@ -66,22 +69,30 @@ getKeyBox = do
   unless (kn == "none") (fail "Unsupported kdf")
   ko <- getPascalString
   unless (ko == "") (fail "Invalid kdf options")
-  count <- fromIntegral <$> getWord32be
-  publicKeys <- replicateM count (runStrictGet getPublicKey <$> getPascalString)
-  privateKeys <- runStrictGet (getPrivateKeys count) <$> getPascalString
-  return $ KeyBox cn kn ko publicKeys (Just privateKeys)
+  count <- getWord32be'
+  publicData <- getPascalString
+  privateData <- getPascalString
+  return $ KeyBox cn kn ko count publicData privateData
 
-getPublicKey :: Get Key
-getPublicKey = PublicKey <$> getPascalString <*> getPascalString
+publicKeys :: KeyBox -> [PublicKey]
+publicKeys box = flip runStrictGet (boxPublicKeys box) $
+  replicateM (keycount box) $ do
+    keyType <- getPascalString
+    case keyType of
+     "ssh-ed25519" -> Ed25519PublicKey <$> getPascalString
+     _ -> fail "Unsupported key type"
 
-getPrivateKey :: Get Key
-getPrivateKey = PrivateKey
-  <$> getPascalString
-  <*> getPascalString
-  <*> getPascalString
-  <*> getPascalString
+getPrivateKey :: Get PrivateKey
+getPrivateKey = do
+  keyType <- getPascalString
+  case keyType of
+   "ssh-ed25519" -> Ed25519PrivateKey
+                    <$> (Ed25519PublicKey <$> getPascalString)
+                    <*> getPascalString
+                    <*> getPascalString
+   _ -> fail "Unsupported key type"
 
-getPrivateKeys :: Int -> Get [Key]
+getPrivateKeys :: Int -> Get [PrivateKey]
 getPrivateKeys count = do
   checkint1 <- getWord32be
   checkint2 <- getWord32be
@@ -90,6 +101,11 @@ getPrivateKeys count = do
   padding <- toStrict <$> getRemainingLazyByteString
   unless (B.take (B.length padding) expected_padding == padding) (fail "Incorrect padding")
   return keys
+
+privateKeys :: KeyBox -> [PrivateKey]
+privateKeys box | ciphername box == "none" =
+                    runStrictGet (getPrivateKeys $ keycount box) (boxPrivateKeys box)
+                | otherwise = error "Unsupported encryption type"
 
 parseKey :: BC.ByteString -> Either String KeyBox
 parseKey = fmap (runStrictGet getKeyBox) . dearmorPrivateKey
