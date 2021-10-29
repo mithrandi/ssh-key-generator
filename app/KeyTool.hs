803d15f929abbb6308faf64ddb6bd2adef8b6002
LuoChen <luochen1990@gmail.com>
2021-10-29 18:41:08 +0800

    Improve the cli command, now support hex literal seed

--- app/KeyTool.hs
+++ app/KeyTool.hs
@@ -9,19 +9,26 @@ import           SSH.Key.Derived (deriveKey)
 import           System.Directory (doesFileExist)
 import           System.Entropy (getEntropy)
 import           System.Posix.Files (setFileCreationMask, groupModes, otherModes, unionFileModes)
+import           Data.ByteString.Base16 (decodeBase16)
+import           Data.Either (isLeft, fromRight)
 
 data Args = Args
-            { argsSeed :: FilePath
+            { argsSeed :: String
             , argsMode :: Modes
             , argsOutput :: FilePath
-            , argsHandle :: String
+            , argsComment :: String
             }
 
-data Modes = Raw | Generate | PrivateKey
+data Modes = Hex | Raw | Generate | PrivateKey
 
 parseMode :: String -> Either String Modes
+parseMode "hex-literal" = Right Hex
+parseMode "hex" = Right Hex
+parseMode "raw-seed-file" = Right Raw
 parseMode "raw" = Right Raw
+parseMode "generate-randomly" = Right Generate
 parseMode "generate" = Right Generate
+parseMode "key-file" = Right PrivateKey
 parseMode "key" = Right PrivateKey
 parseMode _ = Left "unknown mode"
 
@@ -33,25 +40,31 @@ parseArgs = Args
   <$> strOption
       ( long "seed"
      <> short 's'
-     <> metavar "SEEDFILE"
-     <> help "File the seed is stored in" )
+     <> metavar "SEED"
+     <> help "Content of the seed, file path or string literal (depends on mode)" )
   <*> option mode
       ( long "mode"
      <> short 'm'
      <> metavar "MODE"
-     <> help "Mode to handle the seed in: raw, generate, key" )
+     <> help "Mode to get the seed: hex-literal (default), raw-seed-file, generate-randomly , key-file" )
   <*> strOption
       ( long "output"
      <> short 'o'
      <> metavar "OUTPUT"
-     <> help "File the generated key is stored in" )
+     <> help "Path of file the generated key is stored in, file name should be specified also" )
   <*> strOption
-      ( long "handle"
-     <> short 'h'
-     <> metavar "HANDLE"
-     <> help "The key handle to use" )
+      ( long "comment"
+     <> short 'c'
+     <> metavar "COMMENT"
+     <> help "The key comment to use, also used as salt" )
 
-getSeed :: Modes -> FilePath -> IO B.ByteString
+getSeed :: Modes -> String -> IO B.ByteString
+getSeed Hex seedString = do
+  let eseed = decodeBase16 (BC.pack seedString)
+  when (isLeft eseed) (fail "invalid hex literal, a valid hex literal should like 0a77ff")
+  let seed = fromRight undefined eseed
+  when (B.length seed < 32) (fail "Seed is shorter than 32 bytes")
+  return seed
 getSeed Raw seedFile = do
   seed <- B.readFile seedFile
   when (B.length seed < 32) (fail "Seed is shorter than 32 bytes")
@@ -72,7 +85,7 @@ main = do
     fullDesc
     <> progDesc "Generate an Ed25519 SSH key deterministically"
     <> header "ssh-key-generator - a deterministic SSH key generator")
-  let handle = BC.pack (argsHandle args)
+  let handle = BC.pack (argsComment args)
   seed <- getSeed (argsMode args) (argsSeed args)
   _ <- setFileCreationMask $ groupModes `unionFileModes` otherModes
   B.writeFile (argsOutput args) . serialiseKey . snd $ deriveKey seed handle
